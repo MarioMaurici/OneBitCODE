062837953041831a4be3023580d6a972ef9e857e
Mario Maurici <mariomito4001@gmail.com>
2025-04-12 18:05:20 -0300

    exerciciosArray

---
+++ JavaScript/IIII/Anotacoes/Arrow Functions/index.js
@@ -0,0 +1,20 @@
+function normalSum(a,b){
+    return a + b
+}
+
+let anonymousSum = function (a,b){
+    return a + b
+}
+
+const ArrowSum = (a,b) => {
+    return a + b
+}
+console.log(ArrowSum)
+
+const ArrowSum2 = (a,b) => a+b
+console.log(ArrowSum2)
+
+//caso aja apenas um parametro
+
+const double = n => `O dobro de ${n} é ${n *2}`
+
---
+++ JavaScript/IIII/Anotacoes/Template Literals/index.js
@@ -0,0 +1,4 @@
+let str = `Essa linha será
+quebrada automaticamente
+ e essa quebrará e terá um espaço no começo
+Resultado de uma expressão ${2+2}`
